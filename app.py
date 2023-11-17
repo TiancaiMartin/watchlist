071f3f29387c6ffa74a50ca8857e4ae70c26f13c
Martin Zhong <martinzhongdt@gmail.com>
2023-11-17 19:48:50 +0800

    Add index page

--- app.py
+++ app.py
@@ -5,6 +5,33 @@ from flask import Flask, render_template
 
 from flask import url_for
 
+from faker import Faker
+#用于自动生成假数据
+from faker.providers import BaseProvider
+from lorem_text import lorem
+import random
+
+movie = {}
+num_entries = 15 #条目数量
+# 自定义 Provider
+class CustomProvider(BaseProvider):
+
+    def custom_title(self):
+        return lorem.words(3) #生成由三个单词组成的随机英文词组
+    def custom_year(self):
+        return random.randint(1950, 2022)  # 生成 1950 到 2022 年之间的随机年份
+
+fake = Faker()
+fake.add_provider(CustomProvider)
+
+# 批量创建用户数据
+for i in range(num_entries):
+    movie[fake.name()]={
+        'title': fake.custom_title,
+        'year': fake.custom_year()
+    }
+
+#print(movie)
 name = 'Martin Zhong'
 movies = [
 {'title': 'My Neighbor Totoro', 'year': '1988'},
